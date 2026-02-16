361cd4df43b31ae6fb48384e8331081c3ae302ac
Sudikshya Bhattarai <bhattaraisudikshya73@gmail.com>
2026-02-16 13:32:05 +0200

    add

---
+++ isnegative.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+//package sprint
+	func IsNegative(n int) bool {
+    if n < 0 {
+      return true
+    } else {
+      return false
+   }
+}
+ func main(){
+	fmt.Println(IsNegative(6))
+}
+
