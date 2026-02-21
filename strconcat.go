72f12f8ee3cb78cc0783a15c8e3bba03b45a6d64
Sudikshya Bhattarai <bhattaraisudikshya73@gmail.com>
2026-02-21 14:26:23 +0200

    commit

---
+++ strconcat.go
@@ -0,0 +1,14 @@
+package sprint
+
+<<<<<<< HEAD
+func StrConcat (s1, s2, delim string)string {
+=======
+func StrConcat(s1, s2, delim string) string {
+>>>>>>> c9262da081624983c07c6bda60b5c97557e27f58
+
+	show := (s1 + delim + s2)
+
+	return show
+
+}
+
